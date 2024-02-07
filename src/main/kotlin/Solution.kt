cce55f2c31d0aaa8a6ac6c4d262fb2360c617c1f
Ohleesang <jammmmed@gmail.com>
2024-02-07 09:21:31 +0900

    2차원 배열 기초 문제

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,2 +1,48 @@
 class Solution {
+    companion object {
+        const val LOGIN = "login"
+        const val WRONG_PASSWORD = "wrong pw"
+        const val FAIL = "fail"
+    }
+
+    fun solution(id_pw: Array<String>, db: Array<Array<String>>): String {
+        db.forEach { user ->
+            if(id_pw[0] == user[0]){
+                return if(id_pw[1]== user[1]) LOGIN
+                else WRONG_PASSWORD
+            }
+        }
+        return FAIL
+    }
+}
+
+fun main() {
+    val a = Solution()
+    a.solution(
+        arrayOf("meosseugi", "1234"),
+        arrayOf(
+            arrayOf("rardss", "123"),
+            arrayOf("yyoom", "1234"),
+            arrayOf("meosseugi", "1234")
+        )
+    )//login
+
+    a.solution(
+        arrayOf("programmer01", "15789"),
+        arrayOf(
+            arrayOf("programmer02", "111111"),
+            arrayOf("programmer00", "134"),
+            arrayOf("programmer01", "1145")
+        )
+
+    )//wrong pw
+
+    a.solution(
+        arrayOf("rabbit04", "98761"),
+        arrayOf(
+            arrayOf("jaja11", "98761"),
+            arrayOf("krong0313", "29440"),
+            arrayOf("rabbit00", "111333")
+        )
+    )
 }
